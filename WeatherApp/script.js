4e018b4b9efb3272e5131ede1669c9299ab00cc1
Riya Bhandari <bhandari28riya@gmail.com>
2023-07-27 11:25:32 +0530

    Day 22 : Weather App

---
+++ WeatherApp/script.js
@@ -0,0 +1,45 @@
+const inputBox = document.querySelector(".input-box");
+const searchBtn = document.getElementById("searchBtn");
+const weatherImg = document.querySelector(".weather-img");
+const temperature = document.querySelector(".temperature");
+const description = document.querySelector(".description");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("wind-speed");
+
+function changeImg(imgType) {
+  if (imgType == "Rain" || imgType == "Storm") {
+    weatherImg.src = "./assets/storm.png";
+  } else if (imgType == "Clouds") {
+    weatherImg.src = "./assets/clouds.png";
+  } else if (imgType == "Sun" || imgType == "Clear") {
+    weatherImg.src = "./assets/sun.png";
+  } else if (imgType == "Snow") {
+    weatherImg.src = "./assets/snow.png";
+  } else if (imgType == "Mist") {
+    weatherImg.src = "./assets/mist.png";
+  } else {
+    weatherImg.src = "./assets/error-404.png";
+  }
+}
+
+searchBtn.onclick = async () => {
+  let cityName = inputBox.value;
+//   inputBox.value = "";
+
+  const weather = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3d52772c4c8e04df2cd2192980c9c434`
+  ).then((res) => res.json());
+
+  console.log(weather);
+
+  temperature.innerHTML = `${Math.round(
+    weather.main.temp - 273.15
+  )} <sup>°C</sup> `;
+
+  description.innerHTML = `${weather.weather[0].main}`;
+  changeImg(`${weather.weather[0].main}`);
+
+  humidity.innerHTML = `${weather.main.humidity} %`;
+
+  windSpeed.innerHTML = `${weather.wind.speed} Km/H`;
+};
